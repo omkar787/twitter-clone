8f4a941a9f2e2fd136d5fbb4e8ac5340339c0bb2
Omkar Bhoir <omkarbhoir878@gmail.com>
2022-01-09 22:21:38 +0530

    Registration page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom"
+import Home from "./Components/Home";
+import Register from "./Components/Register";
+import Login from "./Components/Login"
+import axios from "axios"
+
+axios.defaults.headers.post["Content-Type"] = 'application/json; charset=utf-8'
+// axios.defaults.baseURL = "http://localhost:5000"
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URI
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App text-white">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signup" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Components/Register.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import { BsTwitter } from 'react-icons/bs'
+import { IoMdClose } from 'react-icons/io'
+import { Navigate } from 'react-router-dom'
+import axios from 'axios'
+
+export default function Register() {
+    const [username, setUsername] = useState(null)
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [registered, setRegistered] = useState(false)
+
+    async function formSubmitHandle(e) {
+        e.preventDefault()
+        // await axios.post("/register", {
+        //     username: "omkar",
+        //     email: "omkar@mail.com",
+        //     password: "12345"
+        // })
+        const { data } = await axios({
+            method: "POST",
+            url: "/register",
+            data: {
+                username: username,
+                email: email,
+                password: password
+            }
+        })
+        console.log(data);
+        setRegistered(data.ok)
+    }
+
+    return (
+        <div className="h-screen bg-[#242D34]">
+            <SignUp formSubmitHandle={formSubmitHandle}
+                setUsername={setUsername}
+                setEmail={setEmail}
+                setPassword={setPassword} />
+
+            {registered && <Navigate replace to="/login" />}
+        </div>
+    )
+}
+
+function SignUp({ formSubmitHandle, setUsername, setEmail, setPassword }) {
+
+
+    const inputBoxCss = "bg-black p-4 rounded-md outline-none w-full border-2 border-gray-700 placeholder:text-slate-500"
+    return (
+        <div className='h-full w-full md:flex md:justify-center md:items-center'>
+            <div className='h-full w-full md:w-[600px] md:h-[592px] bg-black md:rounded-xl'>
+                <div className='p-2 flex align-middle justify-between'>
+                    <div className='pt-1'>
+                        <IoMdClose fill='#fff' size={"1.5rem"} />
+                    </div>
+                    <div>
+                        <BsTwitter fill='#fff' size={"2rem"} />
+                    </div>
+                    <div>
+                        {/* Just a Stud */}
+                    </div>
+                </div>
+
+                <form onSubmit={formSubmitHandle} className='px-6 pt-3 flex flex-col gap-5'>
+                    <div className='text-2xl font-medium'>
+                        Create your account
+                    </div>
+
+                    <div className="py-1">
+                        <input onChange={(e) => {
+                            setUsername(e.target.value)
+                        }} className={inputBoxCss} placeholder='Username' type="text" required />
+                    </div>
+
+                    <div className="py-1">
+                        <input onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}
+                            className={inputBoxCss}
+                            placeholder='Email' type="email" required />
+                    </div>
+
+                    <div className="py-1">
+                        <input onChange={(e) => {
+                            setPassword(e.target.value)
+                        }} className={inputBoxCss} placeholder='Password' type="password" required minLength={5} />
+                    </div>
+                    <div className='px-5 pt-5'>
+                        <button type='submit' className='text-center bg-white text-black w-full text-xl p-2 font-medium rounded-full'>
+                            Next
+                        </button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    )
+}
